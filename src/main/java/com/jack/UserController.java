42f37391a480d7896c451e8310f3980896133730
linyy <1321649726@qq.com>
2019-08-13 10:41:50 +0800

    maven仓库加速

--- src/main/java/com/jack/UserController.java
+++ src/main/java/com/jack/UserController.java
@@ -1,6 +1,5 @@
 package com.jack;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,10 +10,11 @@ import java.util.Map;
 /**
  * @Author LYY
  * @Date 2019/8/13
- */
+        */
 @Controller
+@RequestMapping("/user")
 public class UserController {
-    @RequestMapping
+    @RequestMapping("/get/user")
     @ResponseBody
     public Object getUser(){
         System.out.println("UserController.getUser");
